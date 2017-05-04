3b2f5f8931eb3f2d6b7f884979876aa418cb3e59
shengxiao.guo <shengxiao.guo@bitmain.com>
2017-05-04 10:21:59 +0800

    Fix Flash Gap Just Like 1s

--- led_main.c
+++ led_main.c
@@ -5,37 +5,42 @@
 
 #include "led_control.h"
 
-//#define TEST 1
+//#define TEST 0
 
-int main() {
-  InitLed();
-  TimeData_T cur_time;
+int main()
+{
+    InitLed();
+    TimeData_T cur_time;
 
-  struct tm* p;
-  time_t timep;
-  int tempMin = -1;
-  int tt = 0;
-  while( 1) {
-    time(&timep);
-    p = localtime(&timep);
-    //printf("tempMin=%d\r\n", tempMin);
-    if ( /*p->tm_min == tempMin*/0 ) {
-      FlashOpts(p->tm_min);
-      printf("0");
-    } else {
-      cur_time.hour = p->tm_hour;
-      cur_time.min = p->tm_min;
-#ifdef TEST      
-      cur_time.min = tt++;
+    struct tm* p;
+    time_t timep;
+    int tempMin = -1;
+    int tt = 0;
+    while( 1)
+    {
+        time(&timep);
+        p = localtime(&timep);
+        //printf("tempMin=%d\r\n", tempMin);
+        if ( /*p->tm_min == tempMin*/0 )
+        {
+            FlashOpts(p->tm_min);
+            printf("0");
+        }
+        else
+        {
+            cur_time.hour = p->tm_hour;
+            cur_time.min = p->tm_min;
+#ifdef TEST
+            cur_time.min = tt++;
 #endif
-      SendTime(&cur_time);
-      tempMin = p->tm_min;
-    }
-    //usleep(200000);
-    //usleep(500000);
-    
-  }
+            SendTime(&cur_time);
+            tempMin = p->tm_min;
+        }
+        //usleep(200000);
+        //usleep(500000);
 
-  return 0;
+    }
+
+    return 0;
 
 }
